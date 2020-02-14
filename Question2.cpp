70887cc64b1e5d3711025f4bd4c8231ce2e2262c
bsaakash <11618528+bsaakash@users.noreply.github.com>
2020-02-14 06:29:29 -0500

    Wrote function which uses getSpecialProducts to read input vector from file and write output vector to file.

--- Question2.cpp
+++ Question2.cpp
@@ -32,17 +32,56 @@ vector<int> getSpecialProducts(vector<int> input) {
     }
 
     return output;
+}
 
-
+void computeSpecialProduct(string infile) {
+    ifstream fin (infile);
+    if (! fin.is_open()) {
+        cout << "Unable to open file " << infile << endl;
+    }
+    else {
+        cout << "Opened input file " << infile << endl;
+        vector<int> vin;
+        string line;
+        while (getline(fin, line)) {
+            char * word;
+            word = strtok(const_cast<char *>(line.c_str()), " ,");
+            while (word != nullptr) {
+                vin.push_back(stoi(word));
+                word = strtok(nullptr, " .");
+            }
+        }
+        vector<int> vout = getSpecialProducts(vin);
+//            for (int x : vout) cout << x << " ";
+        string ofname ("vector_outputs.txt");
+        ofstream outfile(ofname);
+        for(vector<int>::const_iterator i = vout.begin(); i != vout.end(); ++i) {
+            outfile << *i << " ";
+        }
+        cout << "Wrote the outputs to " << ofname << endl;
+    }
 }
 
 int main() {
     vector<int> v1{2};
     vector<int> v2{2, 4, 3, 5};
+    vector<int> v3{2, 4, 3, 0};
+    vector<int> v4{2, 0, 3, 0};
     vector<int> out = getSpecialProducts(v1);
     for (int x : out) cout << x << " ";
     out = getSpecialProducts(v2);
     cout << endl;
     for (int x : out) cout << x << " ";
+    out = getSpecialProducts(v3);
+    cout << endl;
+    for (int x : out) cout << x << " ";
+    out = getSpecialProducts(v4);
+    cout << endl;
+    for (int x : out) cout << x << " ";
+    cout << endl;
+
+    string infile ("vector_inputs.txt");
+    computeSpecialProduct(infile);
+
     return 0;
 }
